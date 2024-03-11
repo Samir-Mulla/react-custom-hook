0fc559a42171f474390989dc16dc3d34df31ba5a
Samir Mulla <samirmulla710@gmail.com>
2024-03-11 16:04:01 +0530

    optimized : src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import useBrowserSize from "./hooks/useBrowserSize";
 
 function App() {
+  const browser = useBrowserSize();
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="flex justify-evenly pt-3 px-3 text-balance">
+        <h3 className="md:text-3xl text-lg font-bold underline underline-offset-4 text-left">
+          Browser window width : {browser.width}
+        </h3>
+        <h3 className="md:text-3xl text-lg font-bold underline underline-offset-4 text-right">
+          Browser window height : {browser.height}
+        </h3>
+      </div>
     </>
   );
 }
